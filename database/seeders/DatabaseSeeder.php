036b9b2ab7937547789f3db043dd89353525249e
salim-hosen <salim7cse@gmail.com>
2023-08-21 23:44:44 +0600

    user seeder uncommented

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
-            // UserTableSeeder::class,
+            UserTableSeeder::class,
             ProductTableSeeder::class
         ]);
     }
